bf15214b9a311240a61739689e18240e4d52887e
TERENCE.CHAO <terence.chao@deltaww.com>
2020-04-29 15:17:45 +0800

    prepare for style

--- mdn/popup-info-box/main.js
+++ mdn/popup-info-box/main.js
@@ -2,12 +2,14 @@ class PopUpInfo extends HTMLElement {
     constructor() {
         super();
         const shadow = this.attachShadow({mode: 'open'});
+        const wrapper = document.createElement('span');
         const img = document.createElement('img');
         img.src = "img/default.png";
         const text = document.createElement('span');
         text.textContent = 'Hello there!';
-        shadow.appendChild(img);
-        shadow.appendChild(text);
+        shadow.appendChild(wrapper);
+        wrapper.appendChild(img)
+        wrapper.appendChild(text);
     }
 }
 
