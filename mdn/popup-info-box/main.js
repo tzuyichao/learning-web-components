33f5166cae8d3408c1f44b77c80bf6e1f1b07df1
TERENCE.CHAO <terence.chao@deltaww.com>
2020-04-29 15:30:28 +0800

    partial internal style

--- mdn/popup-info-box/main.js
+++ mdn/popup-info-box/main.js
@@ -3,11 +3,25 @@ class PopUpInfo extends HTMLElement {
         super();
         const shadow = this.attachShadow({mode: 'open'});
         const wrapper = document.createElement('span');
+        wrapper.setAttribute('class', 'wrapper');
+
         const img = document.createElement('img');
         img.src = "img/default.png";
         const text = document.createElement('span');
         text.textContent = 'Hello there!';
+
+        const style = document.createElement('style');
+        style.textContent = `
+            .wrapper {
+                position: relative;
+            }
+            img {
+                width: 1.2rem;
+            }
+        `;
+
         shadow.appendChild(wrapper);
+        shadow.appendChild(style);
         wrapper.appendChild(img)
         wrapper.appendChild(text);
     }
