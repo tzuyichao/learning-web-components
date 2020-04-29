803b82bd92e239e9614550b93d01e888cf8e171f
TERENCE.CHAO <terence.chao@deltaww.com>
2020-04-29 15:39:27 +0800

    more style

--- mdn/popup-info-box/main.js
+++ mdn/popup-info-box/main.js
@@ -7,7 +7,9 @@ class PopUpInfo extends HTMLElement {
 
         const img = document.createElement('img');
         img.src = "img/default.png";
+        img.setAttribute('class', 'icon');
         const text = document.createElement('span');
+        text.setAttribute('class', 'info');
         text.textContent = 'Hello there!';
 
         const style = document.createElement('style');
@@ -15,8 +17,26 @@ class PopUpInfo extends HTMLElement {
             .wrapper {
                 position: relative;
             }
+            .info {
+                font-size: 0.8rem;
+                width: 200px;
+                display: inline-block;
+                border: 1px solid black;
+                padding: 10px;
+                background: white;
+                border-radius: 10px;
+                opacity: 0;
+                transition: 0.6s all;
+                position: absolute;
+                bottom: 20px;
+                left: 10px;
+                z-index: 3;
+            }
             img {
                 width: 1.2rem;
+            }
+            .icon:hover + .info, .icon:focus + .info {
+                opacity: 1;
             }
         `;
 
